54baf0135fc0dbc653b57d9d12df7ac7339b9132
nihalnuriye <nihalnuriyeerturk@gmail.com>
2023-11-30 20:25:02 +0100

    klasör eklendi

---
+++ flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
@@ -0,0 +1,27 @@
+from flask import Flask 
+app = Flask(__name__)
+
+@app.route('/')
+def head():
+     return 'Hello World Nihal'
+
+
+@app.route('/second')
+def second():
+     return 'This is second page'
+
+
+@app.route('/third')
+def third():
+     return 'This is third page'
+
+
+@app.route('/fourth/<string:id>')
+def fourth(id):
+     return f'Id of this page is {id}'
+
+
+if __name__ == '__main__':
+
+    # app.run(debug=True, port=8080)
+      app.run(host= '0.0.0.0', port=8081)
